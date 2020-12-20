24b44ce424ba9123c76a5b02a2a3c6e4f7852427
EdemR <teboldadam@gmail.com>
2020-12-20 17:46:49 +0100

    idk

--- src/Card.java
+++ src/Card.java
@@ -13,7 +13,7 @@ public class Card {
         String tempNumber = this.value == 11 ? "J" : this.value == 12 ? "Q" : this.value == 13 ? "K" : this.value == 14 ? "A" : String.valueOf(this.value);
 
         String tempColor = this.color.equals("Heart") ? "\u2665" : this.color.equals("Clubs") ? "\u2666" : this.color.equals("Spades") ? "\u2664" : "\u2667";
-        String line1 = "  -------------\n" +
+        return "  -------------\n" +
                 "  | " + (this.value != 10 ? tempNumber + " " : tempNumber) + "        |\n" +
                 "  |           |\n" +
                 "  |           |\n" +
